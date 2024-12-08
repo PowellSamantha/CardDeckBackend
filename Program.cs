bf81ad7d18b6770db3af203de8b25c404f7bfc69
PowellSamantha <samantha12909134@gmail.com>
2024-12-08 13:17:17 +0200

    Backend

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardDeckSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deck = CreateDeck();
+            var aceOfSpadesCount = 0;
+            var kingOfHeartsCount = 0;
+
+            Console.WriteLine("Starting the card shuffle simulation...\n");
+
+            // Shuffle the deck randomly using LINQ
+            for (int i = 1; i <= 100; i++) // Adjust iterations
+            {
+                deck = ShuffleDeck(deck);
+
+                foreach (var card in deck)
+                {
+                    if (card == "Ace of Spades")
+                    {
+                        aceOfSpadesCount++;
+                        DisplayHighlightedCard("PDP Lazer", "Ace of Spades", aceOfSpadesCount);
+                    }
+                    else if (card == "King of Hearts")
+                    {
+                        kingOfHeartsCount++;
+                        DisplayHighlightedCard("PDP Lazer", "King of Hearts", kingOfHeartsCount);
+                    }
+                }
+            }
+
+            Console.WriteLine("\n--- Simulation Summary ---");
+            Console.WriteLine($"Ace of Spades triggered: {aceOfSpadesCount} times");
+            Console.WriteLine($"King of Hearts triggered: {kingOfHeartsCount} times");
+        }
+
+        static List<string> CreateDeck()
+        {
+            var suits = new[] { "Hearts", "Diamonds", "Clubs", "Spades" };
+            var ranks = new[] { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+            return (from suit in suits
+                    from rank in ranks
+                    select $"{rank} of {suit}").ToList();
+        }
+
+        static List<string> ShuffleDeck(List<string> deck)
+        {
+            var random = new Random();
+            return deck.OrderBy(card => random.Next()).ToList();
+        }
+
+        static void DisplayHighlightedCard(string companyName, string cardName, int count)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow; // Highlight text in gold
+            Console.WriteLine($"\n***** {DPD Lazer} *****");
+            Console.WriteLine($"** {Ace} TRIGGERED! Count: {count:} **");
+            Console.WriteLine($"** {Spade]} TRIGGERED! Count: {count:} **");
+            Console.WriteLine("*************************\n");
+            Console.ResetColor();
+        }
+    }
+}
